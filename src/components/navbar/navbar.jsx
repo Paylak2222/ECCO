992494627acdd87bffc0013969e7e0b6aaae4974
aghasi89 <albertpetoyan@mail.ru>
2024-12-03 01:08:47 +0400

    make page

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -24,54 +24,56 @@ const Navbar = () => {
       <nav className={styles.navbar}>
         <LogoEcco />
         <ul
-          className={`${styles.menu} ${
-            isMobileMenuOpen ? styles.menuOpen : ""
-          }`}
+          className={`${styles.menu} ${isMobileMenuOpen ? styles.menuOpen : ""
+            }`}
         >
-          <li>About Us</li>
+          <li><button className={`${styles.button}`}>About Us</button></li>
           <li className={styles.dropdown}>
             <button
               onClick={toggleEccoSystem}
-              className={styles.dropdownTrigger}
+              className={`${styles.dropdownTrigger} ${styles.button} ${isEccoSystemOpen ? styles.active : ""}`}
             >
               Resources
-              {isEccoSystemOpen ? (
-                <span className={styles.arrow}>▲</span>
-              ) : (
-                <span className={styles.arrow}>▼</span>
+              <span className={`${styles.arrow} ${isEccoSystemOpen ? styles.rotate : ''}`}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="8" viewBox="0 0 12 8" fill="none">
+                  <path d="M1 1.5L6 6.5L11 1.5" stroke="#3A3D40" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </span>
+              {isEccoSystemOpen && (
+                <ul className={styles.mobileDropdownTwo}>
+                  <li>Standards</li>
+                  <li>Manual</li>
+                  <li>Brand Intake Form</li>
+                </ul>
               )}
             </button>
-            {isEccoSystemOpen && (
-              <ul className={styles.mobileDropdownTwo}>
-                <li>Standards</li>
-                <li>Manual</li>
-                <li>Brand Intake Form</li>
-              </ul>
-            )}
+
           </li>
 
           <li className={styles.dropdown}>
             <button
               onClick={toggleEccoSystemtow}
-              className={styles.dropdownTrigger}
+              className={`${styles.dropdownTrigger} ${styles.button}  ${isEccoSystemtowOpen ? styles.active : ""}`}
             >
               ECCOSystem
-              {isEccoSystemtowOpen ? (
-                <span className={styles.arrow}>▲</span>
-              ) : (
-                <span className={styles.arrow}>▼</span>
+              <span className={`${styles.arrow} ${isEccoSystemtowOpen ? styles.rotate : ''}`}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="8" viewBox="0 0 12 8" fill="none">
+                  <path d="M1 1.5L6 6.5L11 1.5" stroke="#3A3D40" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </span>
+
+              {isEccoSystemtowOpen && (
+                <ul className={styles.mobileDropdown}>
+                  <li>Brands</li>
+                  <li>Retailers</li>
+                  <li>Labs</li>
+                </ul>
               )}
             </button>
-            {isEccoSystemtowOpen && (
-              <ul className={styles.mobileDropdown}>
-                <li>Brands</li>
-                <li>Retailers</li>
-                <li>Labs</li>
-              </ul>
-            )}
+
           </li>
 
-          <li>Education Hub</li>
+          <li><button className={`${styles.button}`}>Education Hub</button></li>
           <li className={styles.donateButton}>Donate</li>
         </ul>
       </nav>
