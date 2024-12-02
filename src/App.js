4e32c007edb397e99f83bd78ac9ebd350dd9d151
andranik92nersisyan <nersisyan9292@gmail.com>
2024-12-02 17:47:13 +0400

    create app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import Navbar from './components/navbar/navbar';
+
+function App() {
+  return (
     <div className="App">
-    </div>
+      <Navbar />
+      </div>
   );
 }
 
