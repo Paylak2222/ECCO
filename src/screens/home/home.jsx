ad4110468f1936ce929f6344cb4fffe0eaf51ed7
Vahan2001 <vahan.avetisyan.2018@mail.ru>
2024-12-03 17:54:47 +0400

    create home screen

--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -1,10 +1,22 @@
+import AboutSEction from "../../components/aboutSection/aboutSection";
+import EducationSection from "../../components/educationSection/educationSection";
 import Header from "../../components/header/Header";
 import style from "./home.module.css";
 
 export default function Home() {
   return (
     <div className={style.main}>
-      <Header />
+      <header>
+        <Header />
+      </header>
+      <section className={style.section__one}>
+        <AboutSEction />
+      </section>
+      <section className={style.section__two}></section>
+      <section className={style.section__three}>
+        <EducationSection />
+      </section>
+      <section className={style.section__four}> </section>
     </div>
   );
 }
