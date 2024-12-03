82690f876da2c49da771ac21328840492a5e6eaa
Vahan2001 <vahan.avetisyan.2018@mail.ru>
2024-12-03 14:01:26 +0400

    create home screen

---
+++ src/components/menu/menu.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import styles from "./menu.module.css";
+
+const Menu = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const [isEccoSystemOpen, setEccoSystemOpen] = useState(false);
+  const [isEccoSystemtowOpen, setEccoSystemtowOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen((prevState) => !prevState);
+  };
+
+  const toggleEccoSystem = () => {
+    setEccoSystemOpen(!isEccoSystemOpen);
+    if (isEccoSystemtowOpen) {
+      setEccoSystemtowOpen(!isEccoSystemtowOpen);
+    }
+  };
+
+  const toggleEccoSystemtow = () => {
+    setEccoSystemtowOpen(!isEccoSystemtowOpen);
+    if (isEccoSystemOpen) {
+      setEccoSystemOpen(!isEccoSystemOpen);
+    }
+  };
+
+  return (
+    <>
+      <ul
+        className={`${styles.menu} ${isMobileMenuOpen ? styles.menuOpen : ""}`}
+      >
+        <li>
+          <button className={`${styles.button}`}>About Us</button>
+        </li>
+        <li className={styles.dropdown}>
+          <button
+            onClick={toggleEccoSystem}
+            className={`${styles.dropdownTrigger} ${styles.button} ${
+              isEccoSystemOpen ? styles.active : ""
+            }`}
+          >
+            Resources
+            <span
+              className={`${styles.arrow} ${
+                isEccoSystemOpen ? styles.rotate : ""
+              }`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="12"
+                height="8"
+                viewBox="0 0 12 8"
+                fill="none"
+              >
+                <path
+                  d="M1 1.5L6 6.5L11 1.5"
+                  stroke="#3A3D40"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </span>
+            {isEccoSystemOpen && (
+              <ul className={styles.mobileDropdownTwo}>
+                <li>Standards</li>
+                <li>Manual</li>
+                <li>Brand Intake Form</li>
+              </ul>
+            )}
+          </button>
+        </li>
+
+        <li className={styles.dropdown}>
+          <button
+            onClick={toggleEccoSystemtow}
+            className={`${styles.dropdownTrigger} ${styles.button}  ${
+              isEccoSystemtowOpen ? styles.active : ""
+            }`}
+          >
+            ECCOSystem
+            <span
+              className={`${styles.arrow} ${
+                isEccoSystemtowOpen ? styles.rotate : ""
+              }`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="12"
+                height="8"
+                viewBox="0 0 12 8"
+                fill="none"
+              >
+                <path
+                  d="M1 1.5L6 6.5L11 1.5"
+                  stroke="#3A3D40"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </span>
+            {isEccoSystemtowOpen && (
+              <ul className={styles.mobileDropdown}>
+                <li>Brands</li>
+                <li>Retailers</li>
+                <li>Labs</li>
+              </ul>
+            )}
+          </button>
+        </li>
+
+        <li>
+          <button className={`${styles.button}`}>Education Hub</button>
+        </li>
+        <li className={styles.donateButton}>
+          <button type="button">Donate</button>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Menu;
